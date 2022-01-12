c538dcd8a7757f8f4ec2ff9a8e899c52d9b00e35
Victor Feitosa <victorfeitosasouza@gmail.com>
2022-01-11 22:52:11 -0300

    Add others datasets

--- experiments/cart_tests.py
+++ experiments/cart_tests.py
@@ -5,9 +5,10 @@ from sklearn.model_selection import train_test_split
 from cart.cart import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
+import json
 
 
-def print_score_sklearn(X_train, y_train):
+def print_score_sklearn(X_train, y_train, X_test, y_test):
     clf = sklearn_tree.DecisionTreeClassifier(criterion="gini", max_depth=5)
     clf.fit(X_train, y_train)
     print("Train accuracy:", round(clf.score(X_train, y_train), 3))
@@ -35,16 +36,29 @@ def plot_graphic(factors, train_accuracy_list, test_accuracy_list, max_depth_lis
     plt.savefig(filename)
 
 
-if __name__ == "__main__":
+def store_results(result_json, filename):
+    with open(filename, 'w') as f:
+        json.dump(result_json, f)
+
+
+def test_dataset(dataset_name, csv_file, column_class_name, columns_to_delete=None, max_depth_tree=5):
+    # Read CSV
     np.set_printoptions(suppress=True)
-    data = pd.read_csv("../data/dry_bean/Dry_Bean_Dataset.csv")
+    data = pd.read_csv(csv_file)
+    if columns_to_delete:
+        data = data.drop(labels=columns_to_delete, axis=1)
+    data = data.astype({column_class_name: str})
 
-    data = data.astype({"Class": str})
+    # Read cols as float
+    cols = list(data.columns)
+    cols.remove(column_class_name)
+    for col in cols:
+        data[col] = data[col].astype(float)
 
-    data['Class'] = data['Class'].rank(method='dense', ascending=False).astype(int)
+    data[column_class_name] = data[column_class_name].rank(method='dense', ascending=False).astype(int)
 
-    X = data.drop(labels='Class', axis=1).to_numpy()
-    y = data['Class'].astype('int').to_numpy() - 1
+    X = data.drop(labels=column_class_name, axis=1).to_numpy()
+    y = data[column_class_name].astype('int').to_numpy() - 1
 
     unique, counts = np.unique(y, return_counts=True)
     dict(zip(unique, counts))
@@ -54,7 +68,7 @@ if __name__ == "__main__":
 
     results = []
     for factor in np.linspace(0.1, 1.0, num=10):
-        dt = DecisionTreeClassifier(max_depth=8)
+        dt = DecisionTreeClassifier(max_depth=max_depth_tree)
         score_train, score_test = dt.get_score(X_train, y_train, X_test, y_test, modified_factor=factor, debug=False)
         print(f"Train/test accuracy for factor {factor}: {score_train}, {score_test}")
         max_depth, max_depth_redundant, wapl, wapl_redundant = dt.get_explainability_metrics()
@@ -70,17 +84,6 @@ if __name__ == "__main__":
             'wapl_redundant': round(wapl_redundant, 3)
         })
 
-    # for factor in np.linspace(0.1, 1.0, num=10):
-    #     from random import uniform
-    #     results.append({
-    #         'factor': factor,
-    #         'train_accuracy': uniform(0.9, 1.0),
-    #         'test_accuracy': uniform(0.9, 1.0),
-    #         'max_depth': uniform(7, 8),
-    #         'max_depth_redundant': uniform(6, 8),
-    #         'wapl': uniform(6, 8),
-    #         'wapl_redundant': uniform(6, 8)
-    #     })
     plot_graphic([x['factor'] for x in results],
                  [x['train_accuracy'] for x in results],
                  [x['test_accuracy'] for x in results],
@@ -88,4 +91,18 @@ if __name__ == "__main__":
                  [x['max_depth_redundant'] for x in results],
                  [x['wapl'] for x in results],
                  [x['wapl_redundant'] for x in results],
-                 filename='../images/dry_bean.png')
+                 filename=f'results/{dataset_name}.png')
+    store_results({'tree_max_depth': max_depth_tree, "results": results},
+                  filename=f"results/{dataset_name}.txt")
+
+
+if __name__ == "__main__":
+    # test_dataset("dry_bean", "../data/dry_bean/Dry_Bean_Dataset.csv", "Class", max_depth_tree=8)
+    # test_dataset("avila", "../data/avila/avila-tr.csv", "Class", max_depth_tree=10)
+    # test_dataset("obs_network", "../data/obs_network/obs_network_dataset.csv", "Class",
+    #              columns_to_delete=['NodeStatus'], max_depth_tree=10)
+    # test_dataset("cardiotocography", "../data/cardiotocography/CTG.csv", "CLASS",
+    #              columns_to_delete=['Tendency', 'A', 'B', 'C', 'D', 'E', 'AD', 'DE', 'LD', 'FS', 'SUSP'],
+    #              max_depth_tree=5)
+    test_dataset("default_credit_card", "../data/default_credit_card/defaults_credit_card.csv",
+                 "default payment next month", columns_to_delete=['ID'], max_depth_tree=5)
